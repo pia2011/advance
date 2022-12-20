f15860ed44673b24149abb9f2a6350c8bd57ea46
pia2011 <sweet_smell@nate.com>
2022-12-20 14:16:48 +0900

    Initial commit

---
+++ src/test/java/hello/advance/AdvanceApplicationTests.java
@@ -0,0 +1,13 @@
+package hello.advance;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AdvanceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
